47d81531c3f16617da42e015f05b880832effef5
Shreya <79649501+24shreya@users.noreply.github.com>
2021-10-10 00:37:14 +0530

    Created SH_broshure.cpp

---
+++ Sherlock Holmes/SH_brochure.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void line()
+{
+	//add code here
+	cout<<"< L - I - N - E >"<<endl;
+}
+int main()
+{
+	line();
+	line();
+	cout<<"\n\t\t\t  * * * SHERLOCK HOLMES * * *\n"<<endl;
+	line();
+	line();
+	cout<<"\n\t\t\t   The Science of Deduction\n"<<endl;
+	line();
+	cout<<"\n\t\t\t      Contact: John Whatson"<<endl;
+	cout<<"\t\t\t\tRetd. Army Doctor\n"<<endl;
+	line();
+	cout<<"\n\t\t\t       221 B, Baker Street"<<endl;
+	cout<<"\t\t\t\t     LONDON\n"<<endl;
+	line();
+	cout<<"\n\t\t\t\t The Game is ON!\n"<<endl;
+	line();
+	line();
+}
