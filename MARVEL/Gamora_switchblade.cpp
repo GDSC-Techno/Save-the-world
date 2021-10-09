f577f85520fcacc6e9d0e7fb5ed3a4fa1b9a9827
Shreya <79649501+24shreya@users.noreply.github.com>
2021-10-10 00:12:48 +0530

    Created Gamora_switchblade.cpp file

---
+++ MARVEL/Gamora_switchblade.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{   
+    cout<<"\n<THANOS'-QUOTE>\n"<<endl;
+    
+	int n=6;
+	for(int i=0; i<n; i++)
+	{
+		for(int k=n-1; k>i; k--)
+		{
+			cout<<" ";
+		}
+		
+		for(int j=0; j<=i; j++)
+		{
+			cout<<"* ";
+		}
+		cout<<endl;
+	}
+	
+	cout<<endl;
+	
+	for(int i=0; i<n+4; i++)
+	{
+		for(int j=0; j<n; j++)
+		{
+			cout<<"= ";
+		}
+		cout<<endl<<endl;
+	}
+	
+	//add code here
+	
+}
